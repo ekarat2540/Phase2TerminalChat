b100538f7956b7107a0135b3cefe9865e583c272
LAPTOP-1E1JPDL3\User <ekarat.suk@hotmail.com>
2024-07-01 10:09:04 +0700

    set code for ip address server bty code like Phase1

--- Phase2TerminalChat/Program.cs
+++ Phase2TerminalChat/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+
+class Sender
+{
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            string ipAddress = "";
+            int Port = 5713;
+            TcpClient receiverClient = new TcpClient();
+            NetworkStream receiverStream = receiverClient.GetStream();
+            StreamWriter writer = new StreamWriter(receiverStream);
+            Console.WriteLine("Enter message here");
+            string message;
+            while((message = Console.ReadLine())  != null)
+            {
+                writer.WriteLine(message);
+                writer.Flush();
+            }
+            writer.Close();
+            receiverStream.Close();
+            receiverClient.Close();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
